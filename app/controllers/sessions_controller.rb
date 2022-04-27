2591f2ae4e84be3809c0373752269017e2682750
zidajoseph <zidajoseph60@gmail.com>
2022-04-27 01:30:50 +0000

    add association

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,13 +9,13 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to user_path(user.id)
     else
-      flash[:danger] = 'I failed to login'
+      # flash[:danger] = 'I failed to login'
       render :new
     end
   end
   def destroy
     session.delete(:user_id)
-    flash[:notice] = 'logged out'
+    # flash[:notice] = 'logged out'
     redirect_to new_session_path
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_20_224150) do
+ActiveRecord::Schema.define(version: 2022_04_26_010619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2022_04_20_224150) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +38,5 @@ ActiveRecord::Schema.define(version: 2022_04_20_224150) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
